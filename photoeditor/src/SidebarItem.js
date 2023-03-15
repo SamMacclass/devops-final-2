f6d5cb964412681a467b58728b0e6c775ec35365
fb0801 <fbhatti0801@gmail.com>
2023-03-15 16:08:00 +0000

    sidebar css

--- photoeditor/src/SidebarItem.js
+++ photoeditor/src/SidebarItem.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SidebarItem() {
   return (
-    <button className='sidebar-item '>Sidebar Item</button>
+    <button className='sidebar-item'>Sidebar Item</button>
   )
 }
